d195e11fe2f3dbd4ed46c21780b069bb800c78a5
Dan Girellini <dan@longhands.org>
2014-12-14 19:16:57 -0800

    Strip out bad params from user creation request

--- src/dumpstr/core/user.clj
+++ src/dumpstr/core/user.clj
@@ -4,21 +4,21 @@
                     [credentials :as creds])
    [dumpstr.core.db :as db]))
 
-(derive ::admin ::user)
+;;(derive ::admin ::user)
+
+(defn valid-user-keys [:username :photo-url :email])
 
 
 (defn create-user [params]
-  ;; TODO: strip out invalid keys
   ;; TODO: check for required keys
   ;; TODO: sanitize input?
   (let [un (:username params)]
     (if (db/get-user un)
       (str "JSON: User already exists")
-      (let [roles (if (zero? (db/num-users)) #{::admin} #{::user})
-            password (creds/hash-bcrypt (:password params))]
-        (db/create-user (assoc params
-                               :roles roles
-                               :password password))
+      (let [roles (if (zero? (db/num-users)) #{:admin} #{:user})
+            password (creds/hash-bcrypt (:password params))
+            user (select-keys params valid-user-keys)]
+        (db/create-user (assoc user :roles roles :password password))
         (str "JSON: I will create user " un " (" (:email params) ")")))))
     
 (defn show-user [user]
