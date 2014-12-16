34572b3c455f1193fbc43d179629b80ed54eb792
Dan Girellini <dan@longhands.org>
2014-12-16 15:02:05 -0800

    Use email as GSI in user table

--- src/dumpstr/core/db.clj
+++ src/dumpstr/core/db.clj
@@ -9,6 +9,7 @@
    :secret-key "SECRET_KEY"
    :endpoint "http://localhost:8000"})
 
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
 (defn create-tables []
   (far/create-table
@@ -16,15 +17,33 @@
    :users
    [:username :s]
    {:throughput {:read 1 :write 1}
-    :block? true}
+    :block? true
+    :gsindexes [{:name "email"
+                 :hash-keydef [:email :s]
+                 :projection :all
+                 :throughput {:read 1 :write 1}}]}
    ))
 
-(defn create-user [user]
-  (far/put-item client-opts :users user))
-
-(defn get-user [username]
-  (far/get-item client-opts :users {:username username}))
 
 (defn num-users []
   (:item-count (far/describe-table client-opts :users)))
+
+(defn create-user
+  [{:keys [username email password photo-url roles] :as request}]
+  (dbg request)
+  (far/put-item client-opts :users
+                (cond-> {:username username
+                         :email email
+                         :password password
+                         :roles (far/freeze roles)
+                         :id (inc (num-users))}
+                  photo-url (assoc :photo-url photo-url))))
+
+(defn get-user [key value]
+  (case key
+    :username
+    (far/get-item client-opts :users {:username value})
+    :email
+    (seq (far/scan client-opts :users
+                   {:attr-conds {:email [:eq value]}}))))
     
