4b7fcf0ec00603de14fefc8eb04ab71aa5a2c97b
Dan Girellini <dan@longhands.org>
2014-12-16 15:14:33 -0800

    More validation in create-user

--- src/dumpstr/core/user.clj
+++ src/dumpstr/core/user.clj
@@ -6,20 +6,30 @@
 
 ;;(derive ::admin ::user)
 
-(defn valid-user-keys [:username :photo-url :email])
+(def valid-user-keys    [:id :username :email :password :photo-url])
+(def required-user-keys [:id :username :email :password])
+
+
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
 
 (defn create-user [params]
-  ;; TODO: check for required keys
-  ;; TODO: sanitize input?
-  (let [un (:username params)]
-    (if (db/get-user un)
-      (str "JSON: User already exists")
-      (let [roles (if (zero? (db/num-users)) #{:admin} #{:user})
-            password (creds/hash-bcrypt (:password params))
-            user (select-keys params valid-user-keys)]
-        (db/create-user (assoc user :roles roles :password password))
-        (str "JSON: I will create user " un " (" (:email params) ")")))))
+  (let [{:keys [username email id]} params
+        resp {:username username :email email :id id}]
+    (cond
+      (db/get-user :username username)
+      (assoc resp :success false :error "Username already exists")
+      (db/get-user :email email)
+      (assoc resp :success false :error "Email already exists")
+;      (db/get-user :id id)
+;      (assoc resp :success false :error "ID already exists")
+      (not (reduce #(and %1 (contains? params %2)) true required-user-keys))
+      (assoc resp :success false :error "Incomplete request")
+      :else (let [roles (if (zero? (db/num-users)) #{:admin} #{:user})
+                  password (creds/hash-bcrypt (:password params))
+                  user (select-keys params valid-user-keys)]
+              (db/create-user (assoc user :roles roles :password password))
+              (assoc resp :success true)))))
     
 (defn show-user [user]
   (str (db/get-user user)))
