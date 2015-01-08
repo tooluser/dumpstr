40e1c80246c627cfbbf147c1f8da62f5aa4a6a43
Dan Girellini <dan@longhands.org>
2015-01-08 09:59:31 -0800

    all-users functionality

--- src/dumpstr/core/admin.clj
+++ src/dumpstr/core/admin.clj
@@ -5,8 +5,26 @@
    [dumpstr.core.db :as db]
    [dumpstr.core.user :as user]))
 
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
 
 (defn show-user [username]
   (let [user (db/get-user :username username)]
     (h/html [:html [:table (for [k (filter #(not= :password %) (keys user))]
                              [:tr [:td k [:td (user k)]]])]])))
+
+(defn get-vals [m [k & ks]]
+  (dbg m)
+  (if (nil? k)
+    nil
+    (cons (m k) (get-vals m ks))))
+
+(def shown-user-fields [:id :username :email :roles :photo-url])
+
+(defn all-users []
+  (let [mkcells (fn [v] (map #(vector :td %) (get-vals v shown-user-fields)))
+        mkrows  (fn [v] (vector :tr (mkcells v)))]
+    (h/html
+     [:table {:border "1"}
+      [:tr (map #(vector :th %) shown-user-fields)]
+        (map mkrows (user/all-users))])))
