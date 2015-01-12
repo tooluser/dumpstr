b8300e9e950e3d6b123449a76cb3c0d0a4b8793c
Dan Girellini <dan@longhands.org>
2015-01-12 11:33:06 -0800

    hack to get version in standalone jar

--- src/dumpstr/core/util.clj
+++ src/dumpstr/core/util.clj
@@ -1,4 +1,6 @@
 (ns dumpstr.core.util)
 
 (def project-version
-  (first (drop 2 (read-string (slurp "project.clj")))))
+  ;; Reading project.clj doen't just work when not running in lein repl
+  ;;  (first (drop 2 (read-string (slurp "project.clj"))))
+  "0.1.0-SNAPSHOT")
